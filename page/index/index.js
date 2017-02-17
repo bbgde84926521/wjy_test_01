0a20afb2a32a254615cb641d2718bd428005d36e
wujingyi <wujingyi@58.com>
2017-02-17 14:59:00 +0800

    测试新建的第一个项目~
    新建第一个自己的项目~

---
+++ page/index/index.js
@@ -0,0 +1,8 @@
+define('page/index/index', function(require, exports, module){
+	//alert('1');
+	module.exports = function(opts){
+		var $ = require('zepto');
+		alert('1');
+		console.log($);
+	}
+})
